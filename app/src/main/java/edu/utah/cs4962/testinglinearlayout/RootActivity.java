59f777c201638477604b726e60d2f3a0a24860f1
zbynek <zzdobrusky@gmail.com>
2014-08-29 14:14:52 -0600

    2 ImageView objects

--- app/src/main/java/edu/utah/cs4962/testinglinearlayout/RootActivity.java
+++ app/src/main/java/edu/utah/cs4962/testinglinearlayout/RootActivity.java
@@ -1,9 +1,9 @@
 package edu.utah.cs4962.testinglinearlayout;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 
@@ -14,7 +14,19 @@ public class RootActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        LinearLayout.LayoutParams rootLayoutParameters = new LinearLayout.LayoutParams(300, 200);
         LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setBackgroundColor(Color.BLACK);
+
+        ImageView img50View = new ImageView(this);
+        img50View.setImageResource(R.drawable.image50);
+        img50View.setBackgroundColor(Color.GREEN);
+        rootLayout.addView(img50View);
+
+        ImageView img100View = new ImageView(this);
+        img100View.setImageResource(R.drawable.image100);
+        img100View.setBackgroundColor(Color.RED);
+        rootLayout.addView(img100View);
 
 
 
