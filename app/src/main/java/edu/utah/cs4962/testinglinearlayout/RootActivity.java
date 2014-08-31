685997f572cd6d81fb825f3c2e23aa2c0c723adc
zbynek <zzdobrusky@gmail.com>
2014-08-31 17:37:53 -0600

    2 ImageView objects

--- app/src/main/java/edu/utah/cs4962/testinglinearlayout/RootActivity.java
+++ app/src/main/java/edu/utah/cs4962/testinglinearlayout/RootActivity.java
@@ -3,6 +3,8 @@ package edu.utah.cs4962.testinglinearlayout;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -14,19 +16,35 @@ public class RootActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        LinearLayout.LayoutParams rootLayoutParameters = new LinearLayout.LayoutParams(300, 200);
+
         LinearLayout rootLayout = new LinearLayout(this);
         rootLayout.setBackgroundColor(Color.BLACK);
 
         ImageView img50View = new ImageView(this);
         img50View.setImageResource(R.drawable.image50);
         img50View.setBackgroundColor(Color.GREEN);
-        rootLayout.addView(img50View);
+        LinearLayout.LayoutParams img50LP = new LinearLayout.LayoutParams(
+                //100, 20, 1);
+                //50, 20, 0);
+                //50, 20, 1);
+                //50, 20, 1);
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+                //ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0);
+        img50LP.gravity = Gravity.LEFT;
+        rootLayout.addView(img50View, img50LP);
 
         ImageView img100View = new ImageView(this);
         img100View.setImageResource(R.drawable.image100);
         img100View.setBackgroundColor(Color.RED);
-        rootLayout.addView(img100View);
+        LinearLayout.LayoutParams img100LP = new LinearLayout.LayoutParams(
+                //100, 20, 2);
+                //100, 20, 0);
+                //100, 20, 1);
+                //100, 20, 1);
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+                //ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+        img100LP.gravity = Gravity.RIGHT;
+        rootLayout.addView(img100View, img100LP);
 
 
 
